065264f5dd445eb939fa7e1847b37be180e78cd6
Simon Graham <sn.graham@outlook.com>
2023-04-08 17:00:49 +0300

    first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+export function About() {
+  return (
+  <>
+  <h1>About</h1>
+  <p>Soon to completely restyle with tailwind, connect redux and query mongoDB</p>
+  </>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <>
+    <h1>Home</h1>
+    <p>Check the store tab for now</p>
+    </>
+    )
+}
